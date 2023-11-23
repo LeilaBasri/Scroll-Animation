390403c76d829545a78b0dd850a8b78c3107b11b
Leila Basri <33898821+LeilaBasri@users.noreply.github.com>
2023-11-22 17:19:17 -0800

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,19 @@
 
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', showBox)
+
+function showBox() {
+    const triggerBottom = window.innerHeight / 1.08;
+    
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+       
+        if(boxTop < triggerBottom){
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+        
+    });
+}
